07d9dbabbf53947eab05e7e2b07c00e1d9fc1add
Jon Roby <jonathan.roby1@gmail.com>
2015-07-21 11:53:56 -0700

    Create Dashboard.js and Main.js

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,33 @@
-'use strict';
-
 var React = require('react-native');
+var Main = require('./App/Components/Main');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
 
-class GithubNotetaker extends React.Component {
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
+
+class githubNotetaker extends React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+      style={styles.container}
+        initialRoute={{
+          title: 'Github NoteTaker',
+          component: Main
+        }} />
     );
   }
 };
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('GithubNotetaker', () => GithubNotetaker);
+AppRegistry.registerComponent('GithubNotetaker', () => githubNotetaker);
