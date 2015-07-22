c637cf4aadfd8da63f2587fb3f155ca63fd88abe
Jon Roby <jonathan.roby1@gmail.com>
2015-07-21 22:15:06 -0700

    Create Notes.js

---
+++ App/Components/Notes.js
@@ -0,0 +1,13 @@
+var React = require('react-native');
+var api = require('../Utils/api');
+var Separator = require('./Helpers/Separator');
+var Badge = require('./Badge');
+
+var {
+  View,
+  Text,
+  ListView,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight
+} = React;
