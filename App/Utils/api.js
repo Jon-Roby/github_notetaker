0cb63ec683994939114a3db5e524f1dc190f7eee
Jon Roby <jonathan.roby1@gmail.com>
2015-07-21 21:55:09 -0700

    Add firebase url to api.js

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -7,9 +7,24 @@ var api = {
   },
 
   getRepos(username) {
-    username = username.toLowerase().trim();
+    username = username.toLowerCase().trim();
     var url = `https://api.github.com/users/${username}/repos`
     return fetch(url).then( (res) => res.json() );
+  },
+
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    var url = `https://github-viewer-react-native.firebaseio.com${username}.json`;
+    return fetch(url).then( (res) => res.json() );
+  },
+
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    var url = `https://github-viewer-react-native.firebaseio.com/${username}.json`
+    return fetch(url, {
+      method: 'post',
+      body: JOSN>stringify(note)
+    }).then( (res) => res.json() );
   }
 };
 
